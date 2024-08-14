9a3cc022c8a25639a7b7f454b3809a511448f9a0
Fernandez Gabriel <152689164+dagulito@users.noreply.github.com>
2024-08-14 23:35:52 +0800

    Add files via upload

---
+++ Chapter 1/Exercise A/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+public class MovieQuoteInfo
+{
+   public static void main(String[] args)
+   {
+      String quote = "Dying is the day worth living for.";
+      String movie = "Pirates of The Carribean";
+      String character = "Hector Barbossa";
+      int year = 2007;
+      
+      System.out.println("Quote: " + quote);
+      System.out.println("Movie: " + movie);
+      System.out.println("Character: " + character);
+      System.out.println("Year: " + year);
+   }
+}
