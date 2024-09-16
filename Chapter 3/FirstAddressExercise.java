958864a725673e42635df0099e3bdd1f332c67ea
Fernandez Gabriel <152689164+Gab-Fernandez@users.noreply.github.com>
2024-09-16 20:12:58 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        System.out.println("First Java application\n");
+
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("Xiaomi Inc.");
+        System.out.println("68 Qinghe Middle St, Haidian District");
+        System.out.println("Beijing, 100085");
+        System.out.println("China");
+    }
+}
