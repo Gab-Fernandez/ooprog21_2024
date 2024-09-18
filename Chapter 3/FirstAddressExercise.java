c0323d5006c356f38b77c6bf59e99b057d416846
Fernandez Gabriel <152689164+Gab-Fernandez@users.noreply.github.com>
2024-09-18 08:56:17 +0800

    Update FirstAddressExercise.java

--- Chapter 3/FirstAddressExercise.java
+++ Chapter 3/FirstAddressExercise.java
@@ -10,6 +10,6 @@ public class FirstAddressExercise {
         System.out.println("Xiaomi Inc.");
         System.out.println("68 Qinghe Middle St, Haidian District");
         System.out.println("Beijing, 100085");
-        System.out.println("China");
+        System.out.println("Company: Xiaomi Inc.");
     }
 }
